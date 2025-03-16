5ee462e0d96309d26b76d8c32eb1929bb2b8685d
Vishank Singh <singhvishank20@gmail.com>
2025-03-16 22:36:23 +0530

    src change in app.py

--- src/app.py
+++ src/app.py
@@ -1,15 +1,12 @@
 from flask import Flask
 import os
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return "Hello, world! This is a Flask app running with CI/CD."
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Use PORT from environment or default to 5000
-    app.run(host="127.0.0.1", port=port) 
+    port = int(os.environ.get("PORT", 8080))  # Use PORT from environment or default to 8080
+    app.run(host="0.0.0.0", port=port)  # ✅ Corrected host
