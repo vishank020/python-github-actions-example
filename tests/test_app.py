99610139dfd4e06ed284f645f5d68e8da372bcfc
Vishank Singh <singhvishank20@gmail.com>
2025-03-16 17:57:58 +0530

    Saving local changes before pulling

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ import os
 # Add the 'src' directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from app import index  # Now this import should work
+from src.app import index  # Now this import should work
 
 def test_index():
     assert index() == "Hello, world! This is a Flask app running with CI/CD."
