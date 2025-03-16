8bda0dbad3da16a3f8bb293c0eef8ef9e76468a0
Vishank Singh <singhvishank20@gmail.com>
2025-03-16 16:27:00 +0530

    Fixed Flake8 and pytest module errors

--- src/app.py
+++ src/app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 import os
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "Hello, world! This is a Flask app running with CI/CD."
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
     app.run(host="0.0.0.0", port=port)
+
 
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,9 @@
-from src.app import index
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from app import index
 
 
 def test_index():
