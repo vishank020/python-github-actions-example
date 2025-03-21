d65fde1e9c0451986b9cfff912054443ae537308
Vishank Singh <singhvishank20@gmail.com>
2025-03-21 12:01:10 +0530

    just changing

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world! This is a Flask app running with CI/CD. hi this is vishank"
+    return "Hello, world! This is a Flask app running with CI/CD. hi this is vishank, how are you doing"
 
 if __name__ == "__main__":
     host = os.environ.get("FLASK_RUN_HOST", "0.0.0.0")  # Default to 0.0.0.0
